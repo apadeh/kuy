3d0c2ee47441003d74fe3f4d2b2bebc699ade500
apadeh <83292376+apadeh@users.noreply.github.com>
2021-08-14 18:12:51 +0700

    Update dgb.sh

--- dgb.sh
+++ dgb.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
 tar xf cpuminer-opt-linux.tar.gz 
-./cpuminer-sse2 -a lyra2z330 -o stratum+tcp://lyra2z330.sea.mine.zpool.ca:4563 -u DHmG4QGatnQqUxAYChVn8aKfywbMrME2As -p c=DGB,zap=PYRK-lyra2z330 -t 2
+./cpuminer-sse2 -a lyra2z330 -o stratum+tcp://lyra2z330.sea.mine.zpool.ca:4563 -u D7W7FDg1DHGs2BJafuuzoqXddH4WG5dpAt -p c=DGB,zap=PYRK-lyra2z330 -t 2
